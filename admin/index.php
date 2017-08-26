9fb1e96f0b540fa15ef5546fa4382c147674deeb
Alessandro <alessandro.balzarini.9@gmail.com>
2017-08-27 01:21:30 +0200

    fixed login problem

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,5 @@
 <?php 
+
 /* INCLUDIAMO IL FILE DI INIZIALIZZAZIONE */
 include_once("init.php");
 
@@ -9,15 +10,14 @@ else $page = $_GET['page'];
 /* CONTROLLA SE L'UTENTE HA FATTO IL LOGIN, IN CASO CONTRARIO -->LOGIN.PHP */
 if (!User::isLogged())
 {
-    header('Location: index.php?page=login');
-    exit();
+    $page = "login";
 }
 
 /* CONTROLLA SE ESISTE UNA PAGINA INIT E LA CARICA */
 if (file_exists('init_pages/init_'.$page.'.php'))
 include('init_pages/init_'.$page.'.php');
 
-/* CONTROLLA SE ESISTE UNA PAGINA*/
+/* CONTROLLA SE ESISTE LA PAGINA CHE VOGLIAMO CARICARE */
 if (file_exists('includes/pages/'.$page.'.php'))
 {
 
@@ -27,33 +27,10 @@ if (file_exists('includes/pages/'.$page.'.php'))
 
     /* CARICA LA PAGINA SELEZIONATA */
     include('includes/pages/'.$page.'.php');
-    
+
     /* CARICA IL FOOTER */
     /* contiene </body> */
     include("includes/pages/footer.php"); 
 
-
-
-
 } 
 ?>
-
-
-
-
-
-
-
-
-
-<?php
-  /*          $Marta = new User();
-            $Marta->username="M";
-            $Marta->password="MM";
-            $Marta->first_name="marta";
-            $Marta->last_name="Audisio";
-            foreach ($Marta as $marta){
-                            echo $marta;
-            }
-            $Marta->save();*/
- ?>
